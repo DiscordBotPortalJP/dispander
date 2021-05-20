2f32d95bf7a2b4752ea3d85eb921428ba8071796
sizumita <sumito@izumita.com>
2021-05-20 20:38:30 +0530

    [Add] delete message when reaction_add

--- dispander/module.py
+++ dispander/module.py
@@ -8,6 +8,11 @@ regex_discord_message_url = (
     '(?!<)https://(ptb.|canary.)?discord(app)?.com/channels/'
     '(?P<guild>[0-9]{18})/(?P<channel>[0-9]{18})/(?P<message>[0-9]{18})(?!>)'
 )
+regex_extra_url = (
+    r'\?base_aid=(?P<base_author_id>[0-9]{18})'
+    '&aid=(?P<author_id>[0-9]{18})'
+    '&extra=(?P<extra_messages>[0-9,]+)'
+)
 
 
 class ExpandDiscordMessageUrl(commands.Cog):
@@ -19,6 +24,32 @@ class ExpandDiscordMessageUrl(commands.Cog):
         if message.author.bot:
             return
         await dispand(message)
+
+    @commands.Cog.listener()
+    async def on_raw_reaction_add(self, payload: discord.RawReactionActionEvent):
+        if str(payload.emoji) != "\U0001f5d1":
+            return
+        if payload.user_id == self.bot.user.id:
+            return
+
+        channel = self.bot.get_channel(payload.channel_id)
+        message = await channel.fetch_message(payload.message_id)
+        if message.author.id != self.bot.user.id:
+            return
+        elif not message.embeds:
+            return
+
+        embed = message.embeds[0]
+        if getattr(embed.author, "url", None) is None:
+            return
+        data = from_jump_url(embed.author.url)
+        if not (data["base_author_id"] == payload.user_id or data["author_id"] == payload.user_id):
+            return
+        await message.delete()
+        for message_id in data["extra_messages"]:
+            extra_message = await channel.fetch_message(message_id)
+            if extra_message is not None:
+                await extra_message.delete()
 
 
 async def dispand(message):
@@ -82,11 +113,29 @@ def make_jump_url(base_message, dispand_message, extra_messages):
     :param extra_messages: 展開する際にでた二つ目以降のメッセージ(e.g. 画像やembed)
     :return: 混入が完了したメッセージリンク
     """
-    return "{0.jump_url}?aid={1.id}&extra={2}".format(
+    # base_aid: メッセージリンクで飛べる最初のメッセージの送信者のid
+    # aid: メッセージリンクを送信したユーザーのid
+    return "{0.jump_url}?base_aid={1.id}&aid={2.id}&extra={3}".format(
         dispand_message,
+        dispand_message.author,
         base_message.author,
         ",".join([str(i.id) for i in extra_messages])
     )
+
+
+def from_jump_url(url):
+    """
+    メッセージリンクから情報を取得します。
+    :param url: メッセージリンク
+    :return: dict
+    """
+    base_url_match = re.match(regex_discord_message_url + regex_extra_url, url)
+    data = base_url_match.groupdict()
+    return {
+        "base_author_id": int(data["base_author_id"]),
+        "author_id": int(data["author_id"]),
+        "extra_messages": [int(_id) for _id in data["extra_messages"].split(",")]
+    }
 
 
 def compose_embed(message):
