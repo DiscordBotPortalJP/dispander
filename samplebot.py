ebba3fb81f043a2e1516792b567f18fe03f67472
1ntegrale9 <1ntegrale9uation@gmail.com>
2023-01-28 04:58:31 +0900

    [Build] 環境を更新

--- samplebot.py
+++ samplebot.py
@@ -1,16 +1,24 @@
 import discord
 from discord.ext import commands
-import os
-import asyncio
-
-bot = commands.Bot(command_prefix='/', intents=discord.Intents().all())
+from os import getenv
 
 
-async def main():
-    async with bot:
-        await bot.load_extension('dispander')
-        await bot.start(os.getenv('DISCORD_BOT_TOKEN'))
+class MyBot(commands.Bot):
+    def __init__(self):
+        super().__init__(
+            command_prefix='$',
+            help_command=None,
+            intents=discord.Intents.all(),
+            application_id=getenv('APPLICATION_ID'),
+        )
+
+    async def setup_hook(self):
+        await self.load_extension('dispander')
+
+
+def main():
+    MyBot().run(getenv('DISCORD_BOT_TOKEN'))
 
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    main()
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="dispander",
-    version="0.5.0",
+    version="1.0.0",
     author="1ntegrale9",
     author_email="1ntegrale9uation@gmail.com",
     description="Discord Message URL Expander",
@@ -14,7 +14,9 @@ setuptools.setup(
     url="https://github.com/DiscordBotPortalJP/dispander",
     packages=setuptools.find_packages(),
     classifiers=[
-        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
